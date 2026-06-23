0d58d467a8ea1616c159ffedc452f2cd14d25557
widad-sksn <indofals12345@gmail.com>
2026-06-24 01:46:03 +0700

    Fix typo in setup_server.sh

--- deployment/setup_server.sh
+++ deployment/setup_server.sh
@@ -3,16 +3,16 @@
 # Script to prepare Ubuntu 24.04 for Odoo + Docker + CI/CD
 
 echo "Updating system..."
-sudo apt-update && sudo apt-upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 echo "Installing required packages (curl, git, ufw)..."
-sudo apt-install -y curl git ufw apt-transport-https ca-certificates software-properties-common
+sudo apt install -y curl git ufw apt-transport-https ca-certificates software-properties-common
 
 echo "Installing Docker & Docker Compose..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-update
-sudo apt-install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 echo "Enabling and starting Docker..."
 sudo systemctl enable docker
